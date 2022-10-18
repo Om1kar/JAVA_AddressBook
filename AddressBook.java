b47dbae8286d5b48ea9191ad4d54817c49b6d246
omkar <omkardudhewar121@gmail.com>
2022-10-18 22:03:11 +0530

    added :contacts created

---
+++ AddressBook.java
@@ -0,0 +1,39 @@
+package AddressBook;
+
+import java.util.Scanner;
+
+public class AddressBook {
+    Scanner sc = new Scanner(System.in);
+
+    Contacts obj = new Contacts();
+
+  public void Contacts() {
+
+        System.out.println("Creating contact");
+        System.out.println("Enter First Name");
+        obj.setFirstName(sc.next());
+        System.out.println("Enter lst Name");
+        obj.setLastName(sc.next());
+        System.out.println("Enter Address");
+        obj.setAddress(sc.next());
+        System.out.println("Enter State");
+        obj.setState(sc.next());
+        System.out.println("Enter Zip");
+        obj.setZip(sc.nextInt());
+        System.out.println("Enter Phone no");
+        obj.setPhoneNo(sc.nextLong());
+        System.out.println("Enter E-mail");
+        obj.setEmail(sc.next());
+
+    }
+
+    public void showContacts() {
+        System.out.println("Enter First Name" + obj.getFirstName());
+        System.out.println("Enter lst Name" + obj.getLastName());
+        System.out.println("Enter Address" + obj.getAddress());
+        System.out.println("Enter State" + obj.getState());
+        System.out.println("Enter Zip" + obj.getZip());
+        System.out.println("Enter Phone no" + obj.getPhoneNo());
+        System.out.println("Enter E-mail" + obj.getEmail());
+    }
+}
---
+++ Contacts.java
@@ -0,0 +1,69 @@
+package AddressBook;
+
+import java.util.Scanner;
+
+public class Contacts {
+ private String firstName;
+    private String lastName;
+    private String address;
+    private String state;
+    private int zip;
+    private long phoneNo;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
